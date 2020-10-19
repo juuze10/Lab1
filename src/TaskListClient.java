284a397be3a6ac437771d11766658a67161b882e
juuze10 <57771877+juuze10@users.noreply.github.com>
2020-10-19 02:03:39 +0100

    criaçao do cliente e servidor
    escleto paraimplementaçao de um cliente servidor

--- src/TaskListClient.java
+++ src/TaskListClient.java
@@ -1,3 +1,19 @@
-public class TaskListClient {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class TaskListClient {
+    public static void main(String[] args) throws Exception {
+        Socket clientSocket = new Socket("127.0.0.0", 4242);//criação de Socket de clt arranca servidor cliente na maquina local
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        out.println("Q");
+        String reponse = in.readLine();
+        System.out.println(reponse);
+        in.close();
+        out.close();
+        clientSocket.close();
+
+    }
 }
--- src/TaskListServer.java
+++ src/TaskListServer.java
@@ -1,16 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.PrintWriter;
 
 public class TaskListServer {
-    ServerSocket serverSocket = new ServerSocket(4242);
-    System.out.println("Servidor iniciado na porta 4242");
-    Socket clientSocket;
-    Socket clientSocket = serverSocket.accept();
-    //tudo bloquado ate ser feita conexão e liberar o accept()
+    public static void main(String[] args) throws Exception {
 
-
-
-
-    
+        ServerSocket serverSocket = new ServerSocket(4242);
+        Socket clientSocket = serverSocket.accept();
+        //tudo bloquado ate ser feita conexão e liberar o accept()
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);//servidor envia informaçao ao cliente
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));//servidor captura o que o cliente envia
+        String command = in.readLine();
+        out.println("Server: received command " + command);//escreve no cliente
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
 }
