ba8005c889d35f4b10de540de0b6550d673e11a6
juuze10 <57771877+juuze10@users.noreply.github.com>
2020-10-26 02:11:58 +0000

    Falta mostrar as listas e criar threads para multiplos clientes.

--- src/TaskListClient.java
+++ src/TaskListClient.java
@@ -2,18 +2,47 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class TaskListClient {
     public static void main(String[] args) throws Exception {
-        Socket clientSocket = new Socket("127.0.0.0", 4242);//criação de Socket de clt arranca servidor cliente na maquina local
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        out.println("Q");
-        String reponse = in.readLine();
-        System.out.println(reponse);
-        in.close();
-        out.close();
-        clientSocket.close();
+        Socket clientSocket = new Socket("127.0.0.1", 4242);//criação de Socket de clt arranca servidor cliente na maquina local
+        Scanner scan = new Scanner(System.in);
 
+        while (true) {
+
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.println("You are the client, Please send a message to the server: ");
+            String clientMessage = scan.nextLine();
+
+            switch (clientMessage) {
+
+
+
+                case "L":
+                    out.write("L");
+                    break;
+
+                case "R":
+                    out.write("R");
+                    String newTask = scan.nextLine();
+                    out.append(newTask);
+                    break;
+
+                case "Q":
+                    out.write("Q");
+
+                    out.flush();
+                    in.close();
+                    out.close();
+
+                    String reponse = in.readLine();
+                    System.out.println(reponse);
+
+            }
+        }
     }
 }
--- src/TaskListServer.java
+++ src/TaskListServer.java
@@ -3,20 +3,55 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TaskListServer {
     public static void main(String[] args) throws Exception {
 
+        ArrayList<String> tasks = new ArrayList(); //lista com os dados do cliente
+
         ServerSocket serverSocket = new ServerSocket(4242);
-        Socket clientSocket = serverSocket.accept();
-        //tudo bloquado ate ser feita conexão e liberar o accept()
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);//servidor envia informaçao ao cliente
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));//servidor captura o que o cliente envia
-        String command = in.readLine();
-        out.println("Server: received command " + command);//escreve no cliente
-        in.close();
-        out.close();
-        clientSocket.close();
-        serverSocket.close();
-    }
-}
+        Socket clientSocket = serverSocket.accept(); //aceita ligação do cliente tudo bloquado ate ser feita conexão e liberar o accept()
+        System.out.println("Client Connected!");
+
+        while (true) {
+
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);//servidor envia informaçao ao cliente
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));//servidor captura o que o cliente envia
+            String command = in.readLine(); //captura da mensagem do cliente
+            switch (command) {
+                case "L":
+                    String showList = Arrays.toString(tasks.toArray());
+                    out.write(showList);
+                    break;
+                case "R":
+                    String newTask = in.readLine();
+                    if (newTask.length() <= 120) {
+                        tasks.add(newTask);
+                        out.println("Task added Succesfully!");
+                    } else {
+                        out.println("You entered more than 120 characters");
+                    }
+                    break;
+                case "Q":
+                    out.println("Server: received command " + command);//escreve no cliente
+                    in.close();
+                    out.flush();
+                    out.close();
+                    clientSocket.close();
+
+                    if (clientSocket.isClosed() == true){
+                        serverSocket.close();
+                    }
+
+                    break;
+
+                default:
+                    System.out.println("Invalid option, please try again");
+
+            }
+
+        }
+    }}
